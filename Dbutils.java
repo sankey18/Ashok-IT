bdfa84bfcd929d1d8f45bf880bd4aa04fb9b959f
sankey18 <sanketbharsawade@gmail.com>
2021-07-21 11:46:20 +0530

    modified files

--- Dbutils.java
+++ Dbutils.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sb01Application {
 
+	public void hello()
+{
+}
 	public static void main(String[] args) {
 		SpringApplication.run(Sb01Application.class, args);
 	}
--- Text.java
+++ Text.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sb01Application {
+public class Text {
+
+	Public void mytest(){
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sb01Application.class, args);
 	}
 
 }
