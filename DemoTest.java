0a936aed6865a8917022750e3d4fac4b4bbf387b
sankey18 <sanketbharsawade@gmail.com>
2021-09-02 10:45:40 +0530

    intial commit

---
+++ DemoTest.java
@@ -0,0 +1,15 @@
+package com.sunbeam.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Text {
+
+	Public void mytest(){
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
